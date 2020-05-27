64a7691ad5bf31b4a46d715a5fcb22704ea739fa
maozhihao <915855132@qq.com>
2020-05-27 14:17:47 +0800

    修改demo名称

--- micro-fast-demo/micro-fast-demo-db/src/main/java/com/micro/fast/demo/db/DemoApplication.java
+++ micro-fast-demo/micro-fast-demo-db/src/main/java/com/micro/fast/demo/db/DemoApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author shoufeng
  */
 @SpringBootApplication
-public class demoApplication {
+public class DemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(demoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
