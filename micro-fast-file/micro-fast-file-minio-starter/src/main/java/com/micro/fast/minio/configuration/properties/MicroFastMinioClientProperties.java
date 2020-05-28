be1c66d06a030474da264e01bc97341129779988
maozhihao <915855132@qq.com>
2020-05-28 13:52:02 +0800

    修改注释

--- micro-fast-file/micro-fast-file-minio-starter/src/main/java/com/micro/fast/minio/configuration/properties/MicroFastMinioClientProperties.java
+++ micro-fast-file/micro-fast-file-minio-starter/src/main/java/com/micro/fast/minio/configuration/properties/MicroFastMinioClientProperties.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
- * seaweedfs配置参数
+ * minio配置参数
  *
  * @author shoufeng
  */
